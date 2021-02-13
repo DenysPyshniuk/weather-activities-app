890337d2a1ed27bc5d6a0ff9856f65c23fcb07b3
DenysPyshniuk <denys.pyshniuk@gmail.com>
2021-02-13 12:46:09 -0800

    Specify type for data

--- weather-app/src/components/day_weather.tsx
+++ weather-app/src/components/day_weather.tsx
@@ -4,7 +4,39 @@ import "./day_weather.css";
 
 const DayWeather = () => {
   const [query, setQuery] = useState("");
-  const [weather, setWeather] = useState({});
+  const [weather, setWeather] = useState({
+    coord: {
+      lon: Number,
+      lat: Number,
+    },
+    weather: [
+      {
+        id: Number,
+        main: String,
+        description: "description",
+        icon: String,
+      },
+    ],
+    base: String,
+    main: {
+      temp: 5,
+    },
+    visibility: 0,
+    wind: {},
+    clouds: {},
+    dt: 0,
+    sys: {
+      type: 0,
+      id: 0,
+      country: String,
+      sunrise: 0,
+      sunset: 0,
+    },
+    timezone: 0,
+    id: 0,
+    name: String,
+    cod: 0,
+  });
 
   const search = async (e: any) => {
     if (e.key == "Enter") {
@@ -49,3 +81,5 @@ const DayWeather = () => {
 };
 
 export default DayWeather;
+
+// {"coord":{"lon":-123.1193,"lat":49.2497},"weather":[{"id":601,"main":"Snow","description":"snow","icon":"13d"}],"base":"stations","main":{"temp":-0.85,"feels_like":-5.25,"temp_min":-1.11,"temp_max":-0.56,"pressure":1011,"humidity":93},"visibility":2414,"wind":{"speed":3.09,"deg":120},"snow":{"1h":0.75},"clouds":{"all":90},"dt":1613247890,"sys":{"type":1,"id":954,"country":"CA","sunrise":1613229864,"sunset":1613266150},"timezone":-28800,"id":6173331,"name":"Vancouver","cod":200}
