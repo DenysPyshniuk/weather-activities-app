1da416db6e9b4f2f62e6fea17a92275626b5dcb8
DenysPyshniuk <denys.pyshniuk@gmail.com>
2021-02-13 14:42:22 -0800

    Comment out Search and lint code

--- weather-app/src/main.tsx
+++ weather-app/src/main.tsx
@@ -1,24 +1,24 @@
-import Search from './components/search'
-import DayWeather from './components/day_weather'
-import Quotes from './components/quotes'
-import Activities from './components/activities'
-import WeekWeather from './components/week_weather'
-import './main.css'
+import Search from "./components/search";
+import DayWeather from "./components/day_weather";
+import Quotes from "./components/quotes";
+import Activities from "./components/activities";
+import WeekWeather from "./components/week_weather";
+import "./main.css";
 
 const Main = () => {
   return (
     <body>
       <Quotes />
-      <div className='main-group'>
-        <div className='left-group container'>
-          <Search />
+      <div className="main-group">
+        <div className="left-group container">
+          {/* <Search /> */}
           <DayWeather />
           <WeekWeather />
         </div>
         <Activities />
       </div>
     </body>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
