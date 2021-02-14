5179c1e9aec31193711fa4b5e8baa72860f99463
Clarence Williams <xwilliams.clarencex@gmail.com>
2021-02-13 19:56:54 -0800

    add navbar

---
+++ weather-app/src/components/navbar.tsx
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <p>Meteorol-OG</p>
+      <div className="nav-right">
+        <button className='menu'></button>
+        <p> February 14, 2021 </p>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
