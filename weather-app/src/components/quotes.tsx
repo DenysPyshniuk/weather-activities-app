dd2f35dc0be740f9c4959d63a93d946656d1d98e
Clarence Williams <xwilliams.clarencex@gmail.com>
2021-02-14 17:04:44 -0800

    add api req for chuck && kanyew/ setState

--- weather-app/src/components/quotes.tsx
+++ weather-app/src/components/quotes.tsx
@@ -1,9 +1,36 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+type QuoteProps = {
+  quote?: string;
+  author?: string;
+  value?: string;
+  chuckQuote?: string;
+  kanyeQuote?: string;
+}
+
+const Quotes = ({ chuckQuote, kanyeQuote }: QuoteProps) => {
+  const [state, setState] = useState({
+    chuckQuote,
+    kanyeQuote
+  });
+
+  useEffect(() => {
+    Promise.all([
+      axios.get<QuoteProps>('https://api.chucknorris.io/jokes/random'),
+      axios.get<QuoteProps>('https://api.kanye.rest')
+    ]).then((all) => {
+      setState({
+        chuckQuote: all[0].data.value,
+        kanyeQuote: all[1].data.quote 
+        })
+      }).catch((err) => console.log(err));
+  }, []);
 
 
-const Quotes = () => {
   return (
     <div className='container quote'>
-      <p>I am a quote</p>
+      <p>{state.chuckQuote}</p>
       <button className='button'>New</button>
     </div>
   )
