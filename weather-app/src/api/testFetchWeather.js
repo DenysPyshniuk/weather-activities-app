b5b0a898f0c29299b01e928be945e363f328a61b
DenysPyshniuk <denys.pyshniuk@gmail.com>
2021-02-15 20:49:14 -0800

    Create a test file to fetch data with promises

---
+++ weather-app/src/api/testFetchWeather.js
@@ -0,0 +1,20 @@
+const axios = require("axios");
+
+axios
+  .get(
+    "http://api.openweathermap.org/data/2.5/weather?q=ncouver&units=metric&APPID=c70d729ed6b851bbd3ff70e4f94e5b0d"
+  )
+  .then((res) => {
+    // console.log(res.data);
+    const coords = res.data.coord;
+    // console.log(coords);
+    return axios.get(
+      `http://api.openweathermap.org/data/2.5/onecall?lat=${coords.lat}&lon=${coords.lon}&units=metric&exclude=minutely&appid=a9af9e3ca381e75b9ef809c0a4263fe6`
+    );
+  })
+  .catch((err) => {
+    console.log("Wrong City!");
+  })
+  .then((res) => {
+    console.log(res.data[0]);
+  });
