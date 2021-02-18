8af9ede23d417c2f70d99656670ee26b6f7a2fc2
zainen <zainensuzuki@gmail.com>
2021-02-18 15:45:38 -0800

    default vancouver weather on load

--- weather-app/src/hooks/useWeatherData.tsx
+++ weather-app/src/hooks/useWeatherData.tsx
@@ -1,8 +1,21 @@
-import {  useState } from "react";
+import { useState, useEffect} from "react";
+import axios from 'axios'
 
 export default function useWeatherData() {
   const [dayWeather, setDayWeather] = useState<DayWeather>();
   const [weekWeather, setWeekWeather] = useState<WeekWeather>()
+  useEffect(() => {
+    axios({
+      method: "post",
+      url: "http://localhost:8001/api/dayweather",
+      data: { query: 'Vancouver' },
+    })
+      .then((res: any) => {
+        setDayWeather(res.data.day);
+        setWeekWeather(res.data.week)
+      })
+      .catch((e: any) => console.log(e));
+  }, [])
 
 
   return { dayWeather, setDayWeather, weekWeather, setWeekWeather }
