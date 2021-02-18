004ad879827b4303244f3b5e91089fa17c177c40
DenysPyshniuk <denys.pyshniuk@gmail.com>
2021-02-17 18:05:48 -0800

    Map through the weekly weather data

--- weather-app/src/components/week_weather.tsx
+++ weather-app/src/components/week_weather.tsx
@@ -2,64 +2,29 @@ import "./week_weather.css";
 
 interface WeekWeatherProps extends WeatherProps<WeekWeather> {}
 
-
 const WeekWeather: React.FC<WeekWeatherProps> = (props) => {
   const { weather } = props;
-  console.log('weekweather:', weather)
-  return (
-    <div className="week-container">
-      <ul className="week-ul">
-        {/* Day Start */}
-        <li className="week-li">
-          <div>
-            <p>MON</p>
-            <pre>
-              Day {5}
-              <sup>&deg;C</sup> Night {1}
-              <sup>&deg;C</sup>
-            </pre>
-            <img
-              className="city-icon"
-              src={`https://openweathermap.org/img/wn/13d@2x.png`}
-              alt={"snow"}
-            />
-            <p id="weather-description">{"snow"}</p>
-          </div>
-        </li>
-        {/* Day End */}
-        <li className="week-li">
-          <div>
-            <p>TUE</p>
-          </div>
-        </li>
-        <li className="week-li">
-          <div>
-            <p>WED</p>
-          </div>
-        </li>
-        <li className="week-li">
-          <div>
-            <p>THR</p>
-          </div>
-        </li>
-        <li className="week-li">
-          <div>
-            <p>FRI</p>
-          </div>
-        </li>
-        <li className="week-li">
-          <div>
-            <p>SAT</p>
-          </div>
-        </li>
-        <li className="week-li">
-          <div>
-            <p>SUN</p>
-          </div>
-        </li>
-      </ul>
+  console.log("weekweather:", weather);
+
+  const forecast = weather?.daily.slice(0, 5).map((weatherOfTheDay) => (
+    <div>
+      <p>MON</p>
+      <pre>
+        Day {weather ? Math.round((weatherOfTheDay.temp as any).day) : "-"}
+        <sup>&deg;C</sup>
+        Night {weather ? Math.round((weatherOfTheDay.temp as any).night) : "-"}
+        <sup>&deg;C</sup>
+      </pre>
+      <img
+        className="city-icon"
+        src={`https://openweathermap.org/img/wn/13d@2x.png`}
+        alt={"snow"}
+      />
+      <p id="weather-description">{"snow"}</p>
     </div>
-  );
+  ));
+
+  return <div className="week-container">{forecast}</div>;
 };
 
 export default WeekWeather;
