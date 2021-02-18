d0fb04a8d3bd50f873a646edd4f58d3def943e40
DenysPyshniuk <denys.pyshniuk@gmail.com>
2021-02-18 12:30:00 -0800

    Fix logic for the weekly weather rendering

--- weather-app/src/components/week_weather.tsx
+++ weather-app/src/components/week_weather.tsx
@@ -6,32 +6,35 @@ const WeekWeather: React.FC<WeekWeatherProps> = (props) => {
   const { weather } = props;
   console.log("weekweather:", weather);
 
-  const forecast = weather?.daily.slice(0, 5).map((oneDayWeather) => (
-    <div className="one-day">
-      <p>
-        {new Date(
-          oneDayWeather.dt * 1000 - weather.timezone_offset
-        ).toLocaleString()}
-      </p>
+  const forecast = weather?.daily.map((oneDayWeather) => {
+    const weekItemDate = new Date(
+      oneDayWeather.dt * 1000 - weather.timezone_offset
+    )
+      .toString()
+      .slice(0, 3);
+    return (
+      <div className="one-day">
+        <p>{weekItemDate}</p>
 
-      <br />
-      <p>
-        Day {weather ? Math.round(oneDayWeather.temp.day) : "-"}
-        <sup>&deg;C</sup>
-      </p>
-      <p>
-        Night {weather ? Math.round(oneDayWeather.temp.night) : "-"}
-        <sup>&deg;C</sup>
-      </p>
-      <br />
-      <img
-        className="city-icon"
-        src={`https://openweathermap.org/img/wn/${oneDayWeather.weather[0].icon}@2x.png`}
-        alt={weather ? oneDayWeather.weather[0].description : "-"}
-      />
-      <p id="weather-description">{oneDayWeather.weather[0].description}</p>
-    </div>
-  ));
+        <br />
+        <p>
+          Day {weather ? Math.round(oneDayWeather.temp.day) : "-"}
+          <sup>&deg;C</sup>
+        </p>
+        <p>
+          Night {weather ? Math.round(oneDayWeather.temp.night) : "-"}
+          <sup>&deg;C</sup>
+        </p>
+        <br />
+        <img
+          className="city-icon"
+          src={`https://openweathermap.org/img/wn/${oneDayWeather.weather[0].icon}@2x.png`}
+          alt={weather ? oneDayWeather.weather[0].description : "-"}
+        />
+        {/* <p id="weather-description">{oneDayWeather.weather[0].description}</p> */}
+      </div>
+    );
+  });
 
   return <div className="week-container">{forecast}</div>;
 };
