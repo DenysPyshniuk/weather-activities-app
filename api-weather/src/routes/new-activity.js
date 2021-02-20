39057994e3eba96daf6bee11d9fe4be05e9040fa
zainen <zainensuzuki@gmail.com>
2021-02-19 17:15:41 -0800

    Route not route

--- api-weather/src/routes/new-activity.js
+++ api-weather/src/routes/new-activity.js
@@ -1,12 +1,12 @@
-const router = require('express').router()
+const router = require('express').Router()
 
 module.exports = db => {
   router.post('/new', (request, response) => {
     console.log(request.data)
-    // db.query(`
-    // INSERT INTO activities (weather_type, activity_type, hi_temp, low_temp, activity_name, activity_description)
-    // VALUES
-    // ($1, $2, $3, $4, $5, $6);
-    // `)
+    db.query(`
+    INSERT INTO activities (weather_type, activity_type, hi_temp, low_temp, activity_name, activity_description)
+    VALUES
+    ($1, $2, $3, $4, $5, $6);
+    `)
   })
 }
