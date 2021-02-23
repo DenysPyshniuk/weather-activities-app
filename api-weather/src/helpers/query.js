5385768a2921adf52d93de77b38e3699ab9e3c5e
DenysPyshniuk <denys.pyshniuk@gmail.com>
2021-02-22 17:03:30 -0800

    Fix split method that not allowed searc two words

--- api-weather/src/helpers/query.js
+++ api-weather/src/helpers/query.js
@@ -1,7 +1,7 @@
 const fixQuery = (q) => {
-  const vet = q.includes(' ') ? q.trim().split(' ') : q
-  return vet
-}
+  // const vet = q.includes(" ") ? q.trim().split(" ") : q;
+  const vet = q.includes(" ") ? q.trim() : q;
+  return vet;
+};
 
-
-module.exports = fixQuery
+module.exports = fixQuery;
